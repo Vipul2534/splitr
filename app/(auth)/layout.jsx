3064e89f773e72505ece4473803223d6f247c5a9
Vipul2534 <vs7117500@gmail.com>
2025-09-02 20:14:57 +0530

    feat: Initial setup with Next.js, Shadcn, Convex, and Clerk auth

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AithLayout = ({children}) => {
+  return (
+    <div className='flex justify-center pt-40'>{children}</div>
+  );
+}
+
+export default AithLayout
---
+++ app/layout.js
@@ -0,0 +1,31 @@
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({subsets:["latin"]});
+
+export const metadata = {
+  title: "Splitr",
+  description: "The smartest way to split expenses with friends",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logos/logo-s.png" size="any"/>
+      </head>
+      <body
+        className={`${inter.className}`}>
+        <ClerkProvider>
+          <ConvexClientProvider>
+            <Header/>
+            <main className="min-h-screen">{children}</main>
+          </ConvexClientProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
