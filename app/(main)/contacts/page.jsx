f6caa501ac1e3a21cb5d055066a70936c856cad9
Vipul2534 <vs7117500@gmail.com>
2025-12-14 13:23:22 +0530

    feat: implement payment reminders, email service, and AI insights jobs

--- app/(main)/contacts/page.jsx
+++ app/(main)/contacts/page.jsx
@@ -6,17 +6,16 @@ import { api } from '@/convex/_generated/api';
 import { useConvexQuery } from '@/hooks/use-convex-query';
 import {  Plus, User, Users } from 'lucide-react';
 import Link from 'next/link';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BarLoader } from 'react-spinners';
 import CreateGroupModal from './_components/create-group-modal';
-import { useRouter } from 'next/navigation';
-
+import { useRouter, useSearchParams } from 'next/navigation';
 const ContactsPage = () => {
   const [isCreateGroupModalOpen, setIsCreateGroupModalOpen] = useState(false);
   const {data, isLoading} = useConvexQuery(api.contacts.getAllContacts);
 
   const router = useRouter();
-
+  const searchParams = useSearchParams();
   useEffect(() => {
     const createGroupParam = searchParams.get("createGroup");
 
